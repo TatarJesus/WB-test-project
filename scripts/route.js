6c28dec887a78ebf4c2a3784b519a30d595384ab
Станислав Зиганшин <sziganshin@web-bee.ru>
2023-07-04 21:46:57 +0300

    Исправления в путях

--- scripts/route.js
+++ scripts/route.js
@@ -2,13 +2,13 @@ const home_path = '/WB-test-project';
 let current_href = '';
 
 const routers = {
-  "/": "../../index.html",
-  "/activity": "../../pages/Activity.html",
-  "/map": "../../pages/Map.html",
-  "/time": "../../pages/Time.html",
-  "/activity/": "../../pages/Activity.html",
-  "/map/": "../../pages/Map.html",
-  "/time/": "../../Time.html",
+  "/": "index.html",
+  "/activity": "pages/Activity.html",
+  "/map": "pages/Map.html",
+  "/time": "pages/Time.html",
+  "/activity/": "pages/Activity.html",
+  "/map/": "pages/Map.html",
+  "/time/": "pages/Time.html",
 };
 
 const routers_full = {
@@ -38,11 +38,11 @@ const handleLocation = async () => {
   // const path = window.location.pathname;
   const path = current_href.slice(current_href.lastIndexOf('/'), current_href.length);
   if (home_path + path !== "/WB-test-project/") {
-    document.innerHTML = await fetch(routers_full[path]).then((data) =>
+    document.innerHTML = await fetch(Window.location.origin + home_path + routers_full[path]).then((data) =>
       data.text()
     );
   } else {
-    document.innerHTML = await fetch(routers["/"]).then((data) => data.text());
+    document.innerHTML = await fetch(window.location.origin + home_path + routers["/"]).then((data) => data.text());
   }
   const html = await fetch(window.location.origin + home_path + routers[path]).then((data) => data.text());
   document.querySelector(".container").innerHTML = html;
