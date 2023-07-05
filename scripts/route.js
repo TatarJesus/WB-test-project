fdd07f831a354b513072c1de1594895df9727d56
Станислав Зиганшин <sziganshin@web-bee.ru>
2023-07-05 11:58:33 +0300

    работа с картой, добавление прелоадера

--- scripts/route.js
+++ scripts/route.js
@@ -1,17 +1,17 @@
-const home_path = '/WB-test-project';
-let current_href = '';
+const home_path = "/WB-test-project";
+let current_href = "";
 
 const routers = {
   "/": "/index.html",
   "/activity": "/pages/Activity.html",
   "/map": "/pages/Map.html",
-  "/time": "/pages/Time.html"
+  "/time": "/pages/Time.html",
 };
 
 const routers_full = {
   "/activity/": "/activity/index.html",
   "/map/": "/map/index.html",
-  "/time/": "/time/index.html"
+  "/time/": "/time/index.html",
 };
 
 document.addEventListener("click", (e) => {
@@ -21,26 +21,51 @@ document.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
+document.addEventListener("DOMContentLoaded", readyMap);
+
+window.addEventListener("load", (event) => {
+  document.querySelector(".map-g").innerHTML = '';
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 7,
+    });
+  }
+});
+
+const readyMap = () => {};
+
 const route = (e) => {
   const href = e.target.href;
-  current_href = window.location.origin + home_path + href.slice(href.lastIndexOf('/'), href.length);
-  if ((current_href !== window.location.href) && ((current_href + '/') !== window.location.href)) {
+  current_href =
+    window.location.origin +
+    home_path +
+    href.slice(href.lastIndexOf("/"), href.length);
+  if (
+    current_href !== window.location.href &&
+    current_href + "/" !== window.location.href
+  ) {
     window.history.pushState({}, "", current_href);
     handleLocation();
   }
 };
 
 const handleLocation = async () => {
-  if (current_href === '') current_href = window.location.href;
-  const path = current_href.replace(window.location.origin + home_path, '');
-  let html = '';
+  if (current_href === "") current_href = window.location.href;
+  const path = current_href.replace(window.location.origin + home_path, "");
+  let html = "";
   if (routers_full.hasOwnProperty(path)) {
-    document.innerHTML = await fetch(window.location.origin + home_path + routers_full[path]).then((data) =>
-      data.text()
-    );
+    document.innerHTML = await fetch(
+      window.location.origin + home_path + routers_full[path]
+    ).then((data) => data.text());
   } else {
-    document.innerHTML = await fetch(window.location.origin + home_path + routers["/"]).then((data) => data.text());
-    html = await fetch(window.location.origin + home_path + routers[path]).then((data) => data.text());
+    document.innerHTML = await fetch(
+      window.location.origin + home_path + routers["/"]
+    ).then((data) => data.text());
+    html = await fetch(window.location.origin + home_path + routers[path]).then(
+      (data) => data.text()
+    );
     document.querySelector(".container").innerHTML = html;
   }
 };
