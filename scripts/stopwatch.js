e9a8aad9d03bd414db047a2f3db301acd93a197f
Станислав Зиганшин <sziganshin@web-bee.ru>
2023-07-05 10:53:00 +0300

    Логи удалены, исправление работы секундомера

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,19 +1,7 @@
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
 let interval;
-let time = new Date();
+let time = new Date().getTime();
 
 const updateTime = () => {
-  seconds++;
-  if (seconds === 60) {
-    minutes++;
-    seconds = 0;
-  }
-  if (minutes === 60) {
-    hours++;
-    minutes = 0;
-  }
   if (window.location.pathname === "/WB-test-project/time" || window.location.pathname === "/WB-test-project/time/") {
     let timer = document.getElementById("timer");
     let now = new Date().getTime();
@@ -21,17 +9,11 @@ const updateTime = () => {
     let hhmmss = new Date(sec * 1000).toISOString().substr(11, 8);
     if (hhmmss.indexOf("00:") === 0) timer.textContent = hhmmss.substr(0);
     else timer.textContent = hhmmss;
-    // timer.textContent = hhmmss.indexOf("00:") === 0 ? hhmmss.substr(0) : hhmmss
-    // timer.textContent = `${hours.toString().padStart(2, "0")}:${minutes
-    //   .toString()
-    //   .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   }
 };
 
 const updateUserTime = () => {
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
+  time = new Date().getTime();
 };
 
 interval = setInterval(updateTime, 1000);
